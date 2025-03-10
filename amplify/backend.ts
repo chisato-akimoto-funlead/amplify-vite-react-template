8f5c46061fc66e729772783155c65449dd510fdc
Your Name <you@example.com>
2025-03-10 11:33:58 +0900

    liveness

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,22 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import * as cdk from 'aws-cdk-lib'
 
-defineBackend({
+export const backend = defineBackend({
   auth,
   data,
 });
+
+const livenessStack = backend.createStack("liveness-stack");
+
+const livenessPolicy = new cdk.aws_iam.Policy(livenessStack, "LivenessPolicy", {
+  statements: [
+    new cdk.aws_iam.PolicyStatement({
+      actions: ["rekognition:StartFaceLivenessSession"],
+      resources: ["*"],
+    }),
+  ],
+});
+backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(livenessPolicy); // allows guest user access
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(livenessPolicy); // allows logged in user access
--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,73 @@
-import { useEffect, useState } from "react";
-import { useAuthenticator } from "@aws-amplify/ui-react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+import { Loader, ThemeProvider } from "@aws-amplify/ui-react";
+import { FaceLivenessDetector } from "@aws-amplify/ui-react-liveness";
+import React from "react";
 
 function App() {
-  const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [createLivenessApiData, setCreateLivenessApiData] = React.useState<{
+    sessionId: string;
+  } | null>(null);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+  React.useEffect(() => {
+    const fetchCreateLiveness: () => Promise<void> = async () => {
+      /*
+       * This should be replaced with a real call to your own backend API
+       */
+      await new Promise((r) => setTimeout(r, 2000));
+      const mockResponse = { sessionId: "mockSessionId" };
+      const data = mockResponse;
+
+      setCreateLivenessApiData(data);
+      setLoading(false);
+    };
+
+    fetchCreateLiveness();
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  const handleAnalysisComplete: () => Promise<void> = async () => {
+    /*
+     * This should be replaced with a real call to your own backend API
+     */
+    if (!createLivenessApiData) {
+      console.error("createLivenessApiData is null");
+      return;
+    }
+    const response = await fetch(
+      `/api/get?sessionId=${createLivenessApiData.sessionId}`
+    );
+    const data = await response.json();
 
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
-  }
+    /*
+     * Note: The isLive flag is not returned from the GetFaceLivenessSession API
+     * This should be returned from your backend based on the score that you
+     * get in response. Based on the return value of your API you can determine what to render next.
+     * Any next steps from an authorization perspective should happen in your backend and you should not rely
+     * on this value for any auth related decisions.
+     */
+    if (data.isLive) {
+      console.log("User is live");
+    } else {
+      console.log("User is not live");
+    }
+  };
 
   return (
-    <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
-            {todo.content}
-          </li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
+    <ThemeProvider>
+      {loading ? (
+        <Loader />
+      ) : createLivenessApiData ? (
+        <FaceLivenessDetector
+          sessionId={createLivenessApiData.sessionId}
+          region="us-east-1"
+          onAnalysisComplete={handleAnalysisComplete}
+          onError={(error) => {
+            console.error(error);
+          }}
+        />
+      ) : (
+        <div>Error: Session data is null</div>
+      )}
+    </ThemeProvider>
   );
 }
 
