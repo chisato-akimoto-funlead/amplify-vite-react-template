f632332f16a2e2b534d0c296b67b33bfc68deda4
chisato.akimoto <chisato.akimoto@funlead.co.jp>
2025-03-27 13:24:30 +0900

    add

--- src/hooks/useMoteCam.ts
+++ src/hooks/useMoteCam.ts
@@ -7,7 +7,7 @@ import { speakMessage } from "../hooks/useSpeech";
 import { useLocale } from "./useLocale";
 import { post } from "aws-amplify/api";
 
-const MODEL_PATH = '/models'
+const MODEL_PATH = '/mode'
 const detectorOptions = new faceapi.TinyFaceDetectorOptions();
 
 type MoteCamType = {
